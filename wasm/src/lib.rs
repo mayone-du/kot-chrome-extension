169716b917d300e036381fc76438c90c2f7db071
mayone-du <cocomayo1201@icloud.com>
2021-11-21 16:35:07 +0900

    Fix calc

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,14 +1,25 @@
 use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen]
+#[derive(Debug)]
+pub struct Times {
+    pub hour: u32,
+    pub minutes: u32,
+}
+
 /// 平日平均で何時間働いているかを返します。
 ///
-/// * `work_time` - 実際に働いた時間
+/// * `work_time_minutes` - 実際に働いた分数
 /// * `work_day_count` - 平日働いた日数
-/// TODO: 小数点の調整
+/// TODO: 小数点の調整, 計算をしっかりやる
+/// TODO: 返り値のTimesに不要な値も必要と言われるから考える
 #[wasm_bindgen]
-pub fn calc_work_avarage(work_time: f32, work_day_count: u32) -> f32 {
-    let calced_avarage = (work_time / work_day_count as f32).round();
-    calced_avarage
+pub fn calc_work_avarage(work_time_minutes: f32, work_day_count: u32) -> Times {
+    let calced_avarage = (work_time_minutes / work_day_count as f32).round();
+    Times {
+        hour: calced_avarage as u32,
+        minutes: calced_avarage as u32,
+    }
 }
 
 #[cfg(test)]
@@ -18,7 +29,8 @@ mod tests {
     #[test]
     fn test_calc_work_avarage() {
         let result = calc_work_avarage(114.0, 14);
-        println!("work time result: {}", result);
-        assert_eq!(result, 8.0);
+        println!("work time result: {:?}", result);
+        assert_eq!(result.hour, 8.0);
+        assert_eq!(result.minutes, 0);
     }
 }
